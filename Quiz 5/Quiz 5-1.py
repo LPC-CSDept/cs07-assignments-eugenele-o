93aefc2731cff2a10aa1499759796d8ce864959d
eugenele <98005819+eugenele-o@users.noreply.github.com>
2022-03-22 22:54:28 +0000

    Added WIP

--- Quiz 5/Quiz 5-1.py
+++ Quiz 5/Quiz 5-1.py
@@ -6,4 +6,10 @@ for i in range(10):
   list1.insert(i, rngVal)
 print(f'Your current list is {list1}')
 
-tinyVal = min.list1()
+tinyval = min(list1)
+list1.insert(0, tinyval)
+print(list1)
+
+list1.pop(tinyval)
+tinyval2 = min(list1)
+print(tinyval2)
