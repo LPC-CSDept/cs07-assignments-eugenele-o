d54602fa3f71520a5ff89ebaad0e679abc6ffa06
eugenele <98005819+eugenele-o@users.noreply.github.com>
2022-03-22 23:17:52 +0000

    Added Bar Graph Functions

---
+++ Quiz 5/Quiz 5-3.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+width = 0.15
+x = np.arange(2)
+fig.ax = plt.subplots()
+ax.bar{x-width*1.5, Math, width}
+ax.legend()
+ax.set_title()
+ax.bar_label()
+ax.set_xticks()
